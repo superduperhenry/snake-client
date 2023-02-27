30cfdfafec97f60a79ad146237ec93a6c66e4817
Henry Truong <henry.trb@gmail.com>
2023-02-26 22:08:05 -0500

    implement handleUserInput to exit if ctrl+c pressed

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 const { connect } = require("./client.js");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === "\u0003") {
+    process.exit();
+  }
+  console.log(`this is my input:`, key);
+};
 console.log("Connecting ...");
 
+setupInput();
 connect();

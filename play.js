d058f408a916833e9bce7ded4409957ffdbdd238
Henry Truong <henry.trb@gmail.com>
2023-02-26 22:12:02 -0500

    refactored and split out input functions

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const { connect } = require("./client.js");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === "\u0003") {
-    process.exit();
-  }
-  console.log(`this is my input:`, key);
-};
 console.log("Connecting ...");
 
 setupInput();

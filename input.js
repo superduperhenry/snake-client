2cc9b30b1735f44fe5bcaebadb4069a263e2cc00
Henry Truong <henry.trb@gmail.com>
2023-02-26 22:27:20 -0500

    connected handle input to connection object

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +14,16 @@ const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
   }
+  if (keys[key]) {
+    connection.write(keys[key]);
+  }
+};
+
+const keys = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -3,5 +3,4 @@ const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 
-setupInput();
-connect();
+setupInput(connect());
